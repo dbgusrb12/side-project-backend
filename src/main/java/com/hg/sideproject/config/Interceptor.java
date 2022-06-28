2653974f47387d715da1b3af8a8660fcba10add8
hyungyu <yhg8484@naver.com>
2022-06-28 22:50:52 +0900

    feat(config): interceptor 구현, web configurer cors 설정 추가 - hg.yu

---
+++ src/main/java/com/hg/sideproject/config/Interceptor.java
@@ -0,0 +1,40 @@
+package com.hg.sideproject.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Interceptor implements HandlerInterceptor {
+
+
+    /**
+     * 컨트롤러 이전에 실행 되는 메서드
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return true;
+    }
+
+    /**
+     * 컨트롤러 이후에 실행 되는 메서드
+     * Dispatcher Servlet 이 화면을 처리하기 전에 동작.
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @param modelAndView
+     * @throws Exception
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    }
+}
