1025c4b0774cc1681a6c6417f610dbe42d04155d
hyungyu <yhg8484@naver.com>
2022-06-29 19:02:20 +0900

    feat(error): Error Handler, Error Response 추가 - hg.yu

---
+++ src/main/java/com/hg/sideproject/response/ErrorResponse.java
@@ -0,0 +1,35 @@
+package com.hg.sideproject.response;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+public class ErrorResponse {
+
+    private HttpStatus status;
+    private int code;
+    private String message;
+
+    private ErrorResponse(HttpStatus status, RuntimeException ex) {
+        this.status = status;
+        this.code = status.value();
+        this.message = ex.getMessage();
+    }
+    private ErrorResponse(HttpStatus status, String message) {
+        this.status = status;
+        this.code = status.value();
+        this.message = message;
+    }
+
+    public static ErrorResponse of(HttpStatus status, String message) {
+        ErrorResponse errorResponse = new ErrorResponse(status, message);
+        return errorResponse;
+    }
+
+    public static ErrorResponse of(HttpStatus status, RuntimeException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(status, ex);
+        return errorResponse;
+    }
+}
